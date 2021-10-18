710a93ef8fca77fcabaf198cb6fff34bcb8dc9f3
vcoronado2014 <vcoronado.alarcon@gmaiil.com>
2021-10-18 18:45:14 -0300

    Primer commit para la clase, se definen las entidades de dominio, se agrega el contexto

--- ArticulosMVC/ArticulosMVC/Models/ArticuloContext.cs
+++ ArticulosMVC/ArticulosMVC/Models/ArticuloContext.cs
@@ -10,7 +10,6 @@ namespace ArticulosMVC.Models
     {
         public ArticuloContext(DbContextOptions<ArticuloContext> options) : base(options)
         {
-
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Producto> Productos { get; set; }
