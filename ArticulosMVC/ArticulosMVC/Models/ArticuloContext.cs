b746d052c2968e4b695f49a3cfd00cfae2e081dd
vcoronado2014 <vcoronado.alarcon@gmaiil.com>
2021-10-18 18:43:10 -0300

    proyecto inicial

---
+++ ArticulosMVC/ArticulosMVC/Models/ArticuloContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticulosMVC.Models
+{
+    public class ArticuloContext : DbContext
+    {
+        public ArticuloContext(DbContextOptions<ArticuloContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
